6a543d1f6bf3c52731c531054a03823afb8901a2
Madeleine <madeleine.loras@gmail.com>
2023-01-31 17:36:11 +0100

    first commit

---
+++ backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '7ay5nj8r',
+    dataset: 'production'
+  }
+})
---
+++ backend/schemas/quotes.js
@@ -0,0 +1,18 @@
+
+export default {
+    name: 'quotes',
+    type: 'document',
+      title: 'Quotes',
+    fields: [
+      {
+        name: 'quote',
+        type: 'string',
+        title: 'Quotes'
+      },
+      {
+        name: 'person',
+        type: 'string',
+        title: 'Person'
+      }
+    ]
+  }
